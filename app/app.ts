f46b53ed78489be69a939f97017e6b900b91868f
gcordido <gcordido@microsoft.com>
2022-09-08 14:12:32 -0700

    Fixed Babylon script rendering issue, deleted old tests, updated webpack and ts config files, and dependencies

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, FreeCamera, HemisphericLight, Vector3, MeshBuilder } from "babylonjs";
+import { Engine, Scene, FreeCamera, HemisphericLight, Vector3, MeshBuilder } from "@babylonjs/core";
 
 export class BasicScene {
     scene: Scene;
@@ -7,25 +7,32 @@ export class BasicScene {
     constructor(){
         const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
         this.engine = new Engine(canvas, true);
-        this.scene = this.CreateScene();
+        this.scene = this.CreateScene(canvas);
 
         this.engine.runRenderLoop(()=>{
             this.scene.render();
         });
     }
 
-    CreateScene(): Scene{
+    CreateScene(canvas: HTMLCanvasElement): Scene{
         const scene = new Scene(this.engine);
 
-        const camera = new FreeCamera("camera1", new Vector3(0,1,0), scene);
+        const camera = new FreeCamera("camera1", new Vector3(0,5,-10), scene);
+        camera.setTarget(Vector3.Zero());
+        camera.attachControl(canvas, true);
 
         const light = new HemisphericLight("light", new Vector3(0,1,0), scene);
+        light.intensity = 0.7;
 
         const sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+        sphere.position.y = 1;
 
         const ground = MeshBuilder.CreateGround("ground", {width: 6, height:6}, scene);
+        
 
         return scene;
     }
 
 }
+
+new BasicScene();
